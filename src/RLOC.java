9128749abdf5284651284807912005926c030e18
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-12-03 03:34:18 +0600

    Update RLOC.java

--- src/RLOC.java
+++ src/RLOC.java
@@ -1,6 +1,21 @@
 public class RLOC {
     // RLOC - Relational & Logical Operators - Conditionas
     public static void main(String[] args) {
-        //https://www.youtube.com/watch?v=fis26HvvDII
+        int a = 4;
+
+        a = a + 2; // a += 2;
+        a++; // increment // a += 1; // a = a + 1;
+        System.out.println(a); // 7
+
+        a = a - 2; // a -= 2;
+        a--; // decrement // a = a-1; // a -= 1;
+        System.out.println(a); // 4
+
+        a = a * 2; // a *= 2;
+        System.out.println(a); // 8
+
+        a = a / 2; // a /= 2;
+        System.out.println(a); // 4
+
     }
 }
