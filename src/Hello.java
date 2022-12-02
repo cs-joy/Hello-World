393c452e867f0c79897c0845362099a2d8272e73
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-12-02 22:06:39 +0600

    Update Hello.java

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,21 @@
 public class Hello {
     public static void main(String[] args) {
+        //System.out.print("Hello World");
+        //System.out.println("Hello Wolrd");
+        //System.out.print("Hello World");
+
         System.out.println("Hello World");
+
+        int num = -5;
+        System.out.println(num);
+
+        long myLong  = 5;
+        System.out.println(myLong);
+
+        Double myDouble = 4.5;
+        System.out.println(myDouble);
+
+        float myFloat = (float)7.3; // allways need to do casting because fractional numbers are double in defualt
+        System.out.println(myFloat);
     }
 }
