5f65f22db430998ac8fdd55adfb41a01c282d644
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-12-02 22:23:15 +0600

    updated class name

--- src/dataTypes.java
+++ src/dataTypes.java
@@ -1,4 +1,4 @@
-public class Hello {
+public class dataTypes {
     public static void main(String[] args) {
         //System.out.print("Hello World");
         //System.out.println("Hello Wolrd");
