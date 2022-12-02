3788e41fceabea4b913c33c885869259635e9d07
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-12-02 23:17:27 +0600

    Update RLOC.java

--- src/RLOC.java
+++ src/RLOC.java
@@ -1,6 +1,6 @@
 public class RLOC {
     // RLOC - Relational & Logical Operators - Conditionas
     public static void main(String[] args) {
-        //
+        //https://www.youtube.com/watch?v=fis26HvvDII
     }
 }
