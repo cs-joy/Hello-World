1b3e4027ed59dd6ac68033fa74e1027a9619311b
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-12-03 03:43:33 +0600

    comparison op.

--- src/RLOC.java
+++ src/RLOC.java
@@ -17,5 +17,35 @@ public class RLOC {
         a = a / 2; // a /= 2;
         System.out.println(a); // 4
 
+
+        // main topic
+        // comparison operator
+        int n1 = 5;
+        int n2 = 3;
+
+        // greater than operator
+        boolean answer = n1 > n2;
+        System.out.println(answer);
+
+        // less than operator
+        boolean result = n1 < n2;
+        System.out.println(result);
+
+        // equality/ equal operator
+        boolean ans= n1 == n2;
+        System.out.println(ans);
+
+        // greater than equal operator
+        boolean isTrue = n1 >= n2;
+        System.out.println(isTrue);
+
+        // less than equal operator
+        boolean res = n1 <= n2;
+        System.out.println(res);
+
+        // not equal to be operator
+        boolean isNotEqual = n1 != n2;
+        System.out.println(isNotEqual);
+
     }
 }
