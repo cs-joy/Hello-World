d33def2940f98ef4cbb4cfb42b64e44de14280f1
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-12-03 08:07:52 +0600

    Create guessNumber.java

---
+++ src/guessNumber.java
@@ -0,0 +1,18 @@
+public class guessNumber {
+    /**
+     * We are going to make a GAME
+     * The concept is: Generating a random number and ask the user to guess the number,
+     * continue asking until you receive the correct number
+     * after 5 times of guessing show a game over message
+     * 
+     * Here is how it works!!
+     * Show a welcome message on the screen
+     * Ask the user's name and say hello to the user
+     * Ask if we should start the game
+     * After receiving a positive answer generate a random number and ask for the user guess
+     * If the user guess is correct show a congratulation message
+     * If the number is not correct, ask again until you receive the correct answer
+     * As a hint, beside the first time, every time that you are asking for a number, tell the user to guess higher or lower
+     * If the user failed 5 times, show a game over message and quit the game
+     */
+}
