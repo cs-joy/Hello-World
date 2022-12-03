c66fcc6051c2799b381858a89537b0f450f324f2
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-12-03 07:20:49 +0600

    definition of for loop

--- src/Loops.java
+++ src/Loops.java
@@ -1,5 +1,14 @@
 public class Loops {
     public static void main(String[] args) {
         System.out.println("Loops Explanation");
+        System.out.println("__________________");
+
+        // for loop
+        for (int i = 0; i<10; i++) {
+            System.out.println("Hello " + i);
+        }
+
+
+
     }
 }
