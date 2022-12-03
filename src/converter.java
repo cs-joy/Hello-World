01c1c1d66a599e429730041cf30500892022d544
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-12-03 09:10:43 +0600

    need to implement

---
+++ src/converter.java
@@ -0,0 +1,12 @@
+
+public class converter {
+    public static void main(String[] args) {
+        System.out.println("Time Converter\n");
+
+        /**
+         * user send us time in seconds our program will convert it to hours:min:sec
+         * as for exmaple: 7445s = 2:04:05
+         */
+        
+    }
+}
