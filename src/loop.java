6eeb145a2b403f95b3732472fd58651a27317770
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-12-04 23:29:40 +0600

    explain `for each` loop

--- src/loop.java
+++ src/loop.java
@@ -8,8 +8,15 @@ public class loop {
             System.out.println("i="+i); // execute 2 times
             // inner loop
             for(int j=1; j<=3; j++) { // execute (i*j) 2*3=6 times
-                System.out.println(j);
+                System.out.println("j="+j);
             }
+        }
+
+        // 2. for each loop
+        System.out.println("for each loop");
+        String[] teachers = {"Mike", "John", "Jim", "Kelvin", "Kally"};
+        for(String name: teachers) {
+            System.out.println(name);
         }
     }
 }
