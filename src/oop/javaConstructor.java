0d87a81093328670b9c3a134485e593ac3fcca05
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-12-05 21:43:27 +0600

    c parameters

--- src/oop/javaConstructor.java
+++ src/oop/javaConstructor.java
@@ -6,17 +6,34 @@ package oop;
 public class javaConstructor {
     int num; // class attribute
 
+    int v;
+
+    /**
     // create class constructor for the `javaConstructor` class
     public javaConstructor() {
-        int numb; // Set the initial value for the class attribute num
+        int num = 10; // Set the initial value for the class attribute num
+
+        //int numb;
+        
+        // initializing via users using parameter
+    }
+    */
+
+    public javaConstructor(int u) {
+        v = u;
     }
 
     public static void main(String[] args) {
-        javaConstructor myObj = new javaConstructor(); // Create an object of class 'javaConstructor' (This will call the constructor)
-        System.out.println(myObj.num);
+        //javaConstructor myObj = new javaConstructor(); // Create an object of class 'javaConstructor' (This will call the constructor)
+        //System.out.println(myObj.num);
 
         //  you are not able to set initial values for object attributes.
-        //int m = myObj.numb = 5;
+        //int m = myObj.numb = 5; // 
 
+        // constructor parameters
+        // so that we can initializing the value of the object attributes.
+        javaConstructor cObj = new javaConstructor(34);
+        int x = cObj.v;
+        System.out.println("v= " + x);
     }
 }
