27bf5a57c28aef2bb0914689996eb9188092b153
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-12-05 14:47:19 +0600

    initial point

---
+++ src/oop/Second.java
@@ -0,0 +1,10 @@
+package oop;
+
+public class Second {
+    public static void main(String[] args) {
+        class_objects cObj = new class_objects();
+        System.out.println(cObj.num);
+        System.out.println(cObj.fValue);
+        System.out.println(cObj.myName);
+    }
+}
---
+++ src/oop/class_objects.java
@@ -0,0 +1,14 @@
+package oop;
+
+public class class_objects {
+    int num = 5;
+    String myName = "Zahangir";
+    double dValue = 3.5;
+    float fValue = (float) dValue;
+    /**
+    public static void main(String[] args) {
+        class_objects myObj = new class_objects();
+        System.out.println(myObj.num);
+    }
+    */
+}
