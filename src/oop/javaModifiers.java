6e3fe238b42ab482d89609623da832e098b581ed
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-12-05 22:25:55 +0600

    Create javaModifiers.java

---
+++ src/oop/javaModifiers.java
@@ -0,0 +1,16 @@
+package oop;
+
+public class javaModifiers {
+    /**
+     * The public keyword is an access modifier, meaning that it is used to set the access level for classes, attributes, methods and constructors.
+        We divide modifiers into two groups:
+
+            * Access Modifiers - controls the access level
+            * Non-Access Modifiers - do not control access level, but provides other functionality
+    
+     * Access Modifiers
+       * public - The code is accessible for all classes
+       * private - The code is only accessible within the declared class
+       * protected - The code is accessible in the same package and subclasses. 
+     */
+}
