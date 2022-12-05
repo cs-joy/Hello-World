5f5b583cf14b2866f58d94b3beafd19c4241f59c
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-12-05 22:29:02 +0600

    Update javaModifiers.java

--- src/oop/javaModifiers.java
+++ src/oop/javaModifiers.java
@@ -11,6 +11,15 @@ public class javaModifiers {
      * Access Modifiers
        * public - The code is accessible for all classes
        * private - The code is only accessible within the declared class
-       * protected - The code is accessible in the same package and subclasses. 
+       * default - The code is only accessible in the same package. This is used when you don't specify a modifier. 
+       * protected - The code is accessible in the same package and subclasses.
+       
+     * Non Access Modifiers
+       * final - Attributes and methods cannot be overridden/modified
+       * static - Attributes and methods belongs to the class, rather than an object
+       * abstract - Can only be used in an abstract class, and can only be used on methods. The method does not have a body, for example abstract void run();. The body is provided by the subclass (inherited from).
+       * transient - Attributes and methods are skipped when serializing the object containing them
+       * synchronized - Methods can only be accessed by one thread at a time
+       * volatile - The value of an attribute is not cached thread-locally, and is always read from the "main memory"
      */
 }
