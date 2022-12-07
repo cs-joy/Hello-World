14d7920a1fd47220ea09ad3acfa3d93c738d191f
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-12-07 22:40:36 +0600

    Update TestSynchronization3.java

--- src/oop/synchronizedModifier/TestSynchronization3.java
+++ src/oop/synchronizedModifier/TestSynchronization3.java
@@ -1,5 +1,39 @@
 package oop.synchronizedModifier;
 
+// example of synchronized method by using annonymous class
+
+class List {
+    synchronized void printList(int number) { // synchronized method
+        for(int i=1; i<=5; i++) {
+            System.out.println(number*i);
+            try {
+                Thread.sleep(400);
+            }
+            catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
+
+
 public class TestSynchronization3 {
-    
+    public static void main(String[] args) {
+        final List obj = new List();
+
+        Thread t1 = new Thread() {
+            public void run() {
+                obj.printList(5);
+            }
+        };
+
+        Thread t2 = new Thread() {
+            public void run() {
+                obj.printList(100);
+            }
+        };
+
+        t1.start();
+        t2.start();
+    }
 }
