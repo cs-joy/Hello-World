4f1efb6681d3b5fec434387f744f0d102e7eb6ab
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-12-07 19:22:59 +0600

    transient

--- src/oop/transientModifier/transientKeyword.java
+++ src/oop/transientModifier/transientKeyword.java
@@ -1,5 +1,9 @@
 package oop.transientModifier;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
 class transientKeyword implements Serializable {
@@ -16,5 +20,21 @@ class transientKeyword implements Serializable {
 
     public static void main(String[] args) throws Exception{
         transientKeyword input = new transientKeyword();
+
+        // serialization
+        FileOutputStream fos = new FileOutputStream("abc.txt");
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(input);
+
+        // deserialization
+        FileInputStream fis = new FileInputStream("abc.txt");
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        transientKeyword output = (transientKeyword)ois.readObject();
+
+        System.out.println("i= " + output.i);
+        System.out.println("j= " + output.j);
+        //System.out.println("k= " + output.k);
+        System.out.println("l= " + output.l);
+        System.out.println("m= " + output.m);
     }
 }
